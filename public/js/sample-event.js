895dc0bd19f5faa58ae0a72ad4c09e613675b784
Max Campos <mcampos2a@gmail.com>
2025-04-06 17:52:07 -0700

    added calculation for well info form and added sample event checklist

--- public/js/sample-event.js
+++ public/js/sample-event.js
@@ -1,14 +1,15 @@
 document.querySelector('#calc').addEventListener('click', calcWaterLevel)
 
 function calcWaterLevel(){
-  let correction = document.querySelector('.correction').value
   let swl = document.querySelector('.swl').value
+  let correction = document.querySelector('.correction').value
+  
 
   console.log(correction)
   console.log(swl)
   let corretedSWL = Number(swl) + Number(correction)
 
-  document.querySelector('#show-cswl').innerText = corretedSWL
+  document.querySelector('.cSWL').value = corretedSWL
 
   console.log(corretedSWL)
 }
