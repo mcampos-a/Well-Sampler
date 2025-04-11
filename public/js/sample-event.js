0a737c4b2e05e794f076817900491768339d8351
Max Campos <mcampos2a@gmail.com>
2025-04-10 18:17:28 -0700

    added pump time calc and average discharge calc

--- public/js/sample-event.js
+++ public/js/sample-event.js
@@ -13,3 +13,48 @@ function calcWaterLevel(){
 
   console.log(corretedSWL)
 }
+
+function calcTotalizerTotal(){
+  let totalizerStart = document.querySelector('.totalizerStart').value
+  let totalizerEnd = document.querySelector('.totalizerEnd').value
+
+  let totalizerTotal = Number(totalizerEnd) - Number(totalizerStart)
+
+  document.querySelector('.totalizerTotal').value = totalizerTotal
+}
+
+function calcTimeTotal(){
+  let timeStart = document.querySelector('.timeStart').value
+  let timeEnd = document.querySelector('.timeEnd').value
+
+  timeStartArr = timeStart.split(":")
+  timeStartInMinutes = parseInt(timeStartArr[0]) * 60 + parseInt(timeStartArr[1]) //turn the start time into a sum of minutes
+
+  timeEndArr = timeEnd.split(":")
+  timeEndInMinutes = parseInt(timeEndArr[0]) * 60 + parseInt(timeEndArr[1]) //turn the end time into a sum minutes
+
+  diff = timeEndInMinutes - timeStartInMinutes
+
+  document.querySelector('.timeTotal').value = diff
+
+}
+
+function calcAverageDischarge(){
+  let totalizerStart = document.querySelector('.totalizerStart').value
+  let totalizerEnd = document.querySelector('.totalizerEnd').value
+
+  let totalizerTotal = Number(totalizerEnd) - Number(totalizerStart)
+
+  let timeStart = document.querySelector('.timeStart').value
+  let timeEnd = document.querySelector('.timeEnd').value
+
+  timeStartArr = timeStart.split(":")
+  timeStartInMinutes = parseInt(timeStartArr[0]) * 60 + parseInt(timeStartArr[1]) //turn the start time into a sum of minutes
+
+  timeEndArr = timeEnd.split(":")
+  timeEndInMinutes = parseInt(timeEndArr[0]) * 60 + parseInt(timeEndArr[1]) //turn the end time into a sum minutes
+
+  diff = timeEndInMinutes - timeStartInMinutes
+
+  document.querySelector('.averageDischarge').value = totalizerTotal / diff
+}
