06a242e27e1cd945da2117f0edd7abe37aaa9ceb
Max Campos <mcampos2a@gmail.com>
2025-06-19 15:26:35 +0200

    update comments

--- routes/main.js
+++ routes/main.js
@@ -6,7 +6,7 @@ const SampleEventController = require("../controllers/sample-event")
 const postsController = require("../controllers/posts")
 const { ensureAuth, ensureGuest } = require("../middleware/auth")
 
-router.get("/", mainController.getIndex) //read only route
+router.get("/", mainController.getIndex) 
 router.get("/profile", ensureAuth, postsController.getProfile)
 router.get("/sample-history", ensureAuth, SampleEventController.getSampleHistory)
 router.get("/login", authController.getLogin)
